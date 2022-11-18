b145a40aeff4e6fbaf0aae271d58a13c6a10ba9e
egorgorban <noreply.egorgorban@inbox.ru>
2022-11-18 20:30:59 +0600

    🔨 extend config

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 import {} from "dotenv/config";
 
 export const Config = {
-    TOKEN: process.env.APP_WEATHER_TOKEN,
-    CITY: process.env.APP_WEATHER_CITY,
+    TOKEN: process.env.APP_WEATHER_TOKEN ?? "token",
+    CITY: process.env.APP_WEATHER_CITY ?? "city",
+    LANG: process.env.APP_WEATHER_LANG ?? "en"
 };
